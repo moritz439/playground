3a36e6b88eb1c2e1e009ccca569c4cde35c1739e
moritz439 <moritz.hipper@gmail.com>
2017-12-07 11:56:07 +0100

    jau

--- src/app/components/navbarentry/navbarentry.component.ts
+++ src/app/components/navbarentry/navbarentry.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnInit, Output, ViewChild} from '@angular/core';
+import {AfterContentChecked, Component, Input, OnInit, Output, ViewChild} from '@angular/core';
 import {FormsModule} from "@angular/forms";
 import {NavbarentryService} from "../../services/navbarentry.service";
 
@@ -7,13 +7,12 @@ import {NavbarentryService} from "../../services/navbarentry.service";
   templateUrl: './navbarentry.component.html',
   styleUrls: ['./navbarentry.component.scss']
 })
-export class NavbarentryComponent implements OnInit {
+export class NavbarentryComponent implements OnInit, AfterContentChecked {
 
   public open = false;
   @Input() header: string;
   @Input() paragraphen: number[];
   @ViewChild('textfield') textfield;
-  public paratext: string;
   public initparas;
 
   constructor(public nes: NavbarentryService) {
@@ -23,22 +22,22 @@ export class NavbarentryComponent implements OnInit {
     this.initparas = this.paragraphen;
   }
 
+  ngAfterContentChecked() {
+
+  }
+
   toggle(): void {
     this.open = !this.open;
   }
 
   updateparas() {
-    console.log('--------');
     let textfeld = new Set(this.nes.getParaArrayFromString(this.textfield.nativeElement.value));
-
     let newparas: number[] = [];
 
     textfeld.forEach(v => {
-      newparas.push(v);
+      if (v) { newparas.push(v); }
     });
-
     this.nes.updateEntry(this.header, newparas);
-
   }
 
 }
